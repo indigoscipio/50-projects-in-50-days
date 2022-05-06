dee8753126257f5db23125acd6562e8225a65f3f
unknown <samueloctavianus97@gmail.com>
2022-05-06 14:30:24 +0700

    day3 completed

--- day3-rotating-navigations/day3.js
+++ day3-rotating-navigations/day3.js
@@ -1,1 +1,17 @@
 "use strict";
+
+let mainContainer = document.querySelector(".main-container");
+let menuReveal = document.querySelector(".menu-reveal");
+let btnMenu = document.querySelector(".menu");
+let iconMenu = document.querySelector(".icon-menu");
+let iconClose = document.querySelector(".icon-close");
+
+btnMenu.addEventListener("click", () => {
+  //rotate the canvas
+  mainContainer.classList.toggle("container-rotate");
+  menuReveal.classList.toggle("menu-rotate");
+
+  //change the menu
+  iconMenu.classList.toggle("open");
+  iconClose.classList.toggle("hidden");
+});
