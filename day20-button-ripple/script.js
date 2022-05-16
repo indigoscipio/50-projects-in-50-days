92e34918279b6fc0da8dca35fefb9bf32573da6b
unknown <samueloctavianus97@gmail.com>
2022-05-16 14:55:41 +0700

    finished

--- day20-button-ripple/script.js
+++ day20-button-ripple/script.js
@@ -1,20 +1,16 @@
 let button = document.querySelector("#click-me");
-button.addEventListener("click", function (e) {
-  let x = e.clientX;
-  let y = e.clientY;
-
-  console.log(x, y);
-  let buttonTop = e.target.offsetTop;
-  let buttonLeft = e.target.offsetLeft;
-  console.log(buttonTop, buttonLeft);
-
-  let xInside = x - buttonLeft;
-  let yInside = y - buttonTop;
-
-  let circle = document.createElement("span");
-  circle.classList.add("circle");
-  circle.style.top = yInside + "px";
-  circle.style.left = xInside + "px";
-
-  this.appendChild(circle);
+button.addEventListener("click", (e) => {
+  createRipple(e);
 });
+
+function createRipple(e) {
+  console.log(button.clientWidth);
+  let newCircle = document.createElement("span");
+  let diameter = Math.max(button.clientWidth, button.clientHeight);
+  let radius = diameter / 2;
+  newCircle.style.width = newCircle.style.height = `${diameter}px`;
+  newCircle.style.left = `${e.clientX - (button.offsetLeft + radius)}px`;
+  newCircle.style.top = `${e.clientY - (button.offsetTop + radius)}px`;
+  newCircle.classList.add("circle");
+  button.appendChild(newCircle);
+}
